9f2a74e49a6ff8aceb57bff66ee5acdf3473ea3f
Cristian <crisventuri@hotmail.com>
2019-05-18 12:25:27 -0300

    commit

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     echo "Hello World";
-    echo "Olá Mundo!!";
 
     ?>
 
@@ -14,40 +13,34 @@
     <header>
     </header>
     <body>
-        <div>
-            <div id="caixa-cadastro" class="float-left">
+        <div align="center">
+            <div id="caixa-cadastro">
                 <h3>Novo Usuário</h3>
                 <form method="POST" action="index.php">
                     <label>Usuário:</label>
-                    <input type="text" name="nome" value="<?= isset($_REQUEST['nome']) ? $_REQUEST['nome'] : '' ?>" required><br>
+                    <input type="text" name="nome" value="" required>
 
                     <label>E-mail:</label><br>
-                    <input type="email" name="email" value="<?= isset($_REQUEST['email']) ? $_REQUEST['email'] : '' ?>" required><br>
+                    <input type="email" name="email" value="" required>
 
-                    <div class="clearfix">
+                    <div>
                         <div class="box">
                             <label>Senha:</label>
                             <input type="password" name="senha" required>
                         </div>
-                        <div class="box">
-                            <label>Confirmação:</label>
-                            <input type="password" name="confirmacao_senha" required>
-                        </div>
                     </div>
-                    <span class="mensagemValidacao"><?= ($mensagemValidacaoCadastro) ?></span>
-                    <input type="submit" name="btnSubmit" class="botao" value="Gravar"><br>
+                    <input type="submit" name="btnSubmit" class="botao" value="Gravar">
                 </form>
             </div>
-            <div id="caixa-login" class="float-left">
+            <div id="caixa-login">
                 <h3>Entrar</h3>
                 <form method="POST" action="index.php">
                     <label>E-mail:</label>
-                    <input type="text" name="email" required><br>
+                    <input type="text" name="email" required>
 
-                    <label>Senha:</label><br>
+                    <label>Senha:</label>
                     <input type="password" name="senha" required>
                     <input type="submit" name="btnSubmit" class="botao" value="Entrar">
-                    <span class="mensagemValidacao"><?= ($mensagemValidacao) ?></span>
                 </form>
             </div>
         </div>
@@ -55,12 +48,24 @@
 </html>
 
 <style>
+    .box {
+        float: left;
+        width: 50%;
+    }
+    .box input{
+        width: 90%;
+    }
+    *{
+        box-sizing: border-box;
+    }
+
+    /* Caixa Login */
     #caixa-login{
         padding: 5px;
         border: 1px solid black;
         width:300px;
         padding-bottom: 30px;
-        margin: 10px 10px 10px 20px;
+        margin: 10px 10px 10px 0px;
         float left;
     }
     #caixa-login form input[type="text"]{
@@ -76,6 +81,7 @@
         float: left;
         margin-left: 10%;
     }
+
     /* Caixa Cadastro */
     #caixa-cadastro{
         padding: 5px;
@@ -84,7 +90,6 @@
         margin: 10px 10px 10px 0px;
         padding-bottom: 30px;
         float left;
-
     }
     #caixa-cadastro input[name="password"]{
         width: 80%;
@@ -95,8 +100,7 @@
     }
     #caixa-cadastro form input[type="submit"]{
         float: right;
-        clear: both;
-        margin-top: 10px;
+        margin-top: 18px;
     }
 
 </style>
