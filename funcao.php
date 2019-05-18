ffedf055d523b7a033ae45a409e1cc9ebc69b382
Cristian <crisventuri@hotmail.com>
2019-05-18 16:08:02 -0300

    commit

--- funcao.php
+++ funcao.php
@@ -1,2 +1,9 @@
 <?php
-    $nome = "Cristian";
+    $nome = "Cristian"." "."Venturi";
+    $pessoas = array("0-zero","1-um","2-dois");
+
+
+
+    var_dump($nome);
+    echo $nome;
+    echo $pessoas[2];
